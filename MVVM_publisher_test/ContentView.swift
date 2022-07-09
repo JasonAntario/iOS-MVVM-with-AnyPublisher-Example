4775e64acee2c09abae6e339cab35e5069990bb7
d.sankovsky <d.sankovsky@hqsoftwarelab.com>
2022-07-09 20:07:03 +0300

    Initial Commit

---
+++ MVVM_publisher_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVM_publisher_test
+//
+//  Created by Dmitry Sankovsky on 9.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVM_publisher_test/MVVM_publisher_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_publisher_testApp.swift
+//  MVVM_publisher_test
+//
+//  Created by Dmitry Sankovsky on 9.07.22.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_publisher_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
